2b14d42202da2bbd15117bcade2879e1ec5c7e6d
muhammedabass <148213100+muhammedabass@users.noreply.github.com>
2023-11-20 10:01:44 +0100

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,26 @@
+note="hello world"
+print(note)
+x=2
+print(x)
+x=2+3
+print(x)
+y=x
+print(x)
+
+y=x
+z=3
+y=x-z
+print(y)
+y=x+z
+print(y)
+y=x-z
+print(y)
+y=x/z
+print(y)
+y=x * z
+print(y)
+
+A="Hello"
+name=" abass"
+greet = A + name
+print(greet)
